4724208056b56114f61902788c1c48cb33d33da5
David Hübner <david.huebner@averbis.com>
2021-12-07 16:52:31 +0100

    Issue #98: Adding `page` and `page_size` parameter to export_text_analysis

--- averbis/core/_rest_client.py
+++ averbis/core/_rest_client.py
@@ -763,9 +763,19 @@ class Process:
         # noinspection PyProtectedMember
         return self.project.client._get_process_state(self.project, self)
 
-    def export_text_analysis(self, annotation_types: str = None) -> dict:
+    def export_text_analysis(
+        self, annotation_types: str = None, page: int = None, page_size: int = None
+    ) -> dict:
         """
         Exports a given text analysis process as a json.
+
+        The parameter `page` and `page_size` can be used to only export a subset of all results.
+        For instance, setting page=1 and page_size=10 will export document 1-10 of the text analysis result.
+        Setting page=2 and page_size=30 will export document 31-60 of the text analysis result.
+
+        :param annotation_types: Optional parameter indicating which types should be returned. Supports wildcard expressions, e.g. "de.averbis.types.*" returns all types with prefix "de.averbis.types"
+        :param page: Optional parameter indicating which batch of pages should be export. Only works in combination with `page_size`.
+        :param page_size: Optional parameter defining how many documents are exported at once. Only works in combination with parameter `page`.
 
         :return: The raw payload of the server response. Future versions of this library may return a better-suited
          representation.
@@ -775,9 +785,19 @@ class Process:
                 "Text analysis export is not supported for platform version 5.x, it is only supported from 6.x onwards."
             )
 
+        if (page is not None and page_size is None) or (page is not None and page_size is None):
+            raise ValueError(
+                f"The argument page={page} and page_size={page_size} need either both be present or absent. But one is absent. "
+            )
+
         # noinspection PyProtectedMember
         return self.project.client._export_text_analysis(
-            self.project.name, self.document_source_name, self.name, annotation_types
+            project=self.project.name,
+            document_source=self.document_source_name,
+            process=self.name,
+            annotation_types=annotation_types,
+            page=page,
+            page_size=page_size,
         )
 
     @experimental_api
@@ -2013,7 +2033,13 @@ class Client:
         return response["payload"]
 
     def _export_text_analysis(
-        self, project: str, document_source: str, process: str, annotation_types: str = None
+        self,
+        project: str,
+        document_source: str,
+        process: str,
+        annotation_types: str = None,
+        page: int = None,
+        page_size: int = None,
     ):
         """
         Use Process.export_text_analysis() instead.
@@ -2021,7 +2047,7 @@ class Client:
         response = self.__request_with_json_response(
             "get",
             f"/v1/textanalysis/projects/{project}/documentSources/{document_source}/processes/{process}/export",
-            params={"annotationTypes": annotation_types},
+            params={"annotationTypes": annotation_types, "page": page, "pageSize": page_size},
             headers={HEADER_ACCEPT: MEDIA_TYPE_APPLICATION_JSON},
         )
         return response["payload"]
